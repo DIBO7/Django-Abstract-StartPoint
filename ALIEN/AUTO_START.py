e7c30bf43c10b0a048a9dd262ba3e49920c0a302
Ibra-Portfolio <ibrahim.oduola007@gmail.com>
2020-02-03 12:54:58 +0000

    caught 2 errors and fixed them both - one with the Secret Key Generator and two with reusage/rewriting functions

--- ALIEN/AUTO_START.py
+++ ALIEN/AUTO_START.py
@@ -23,21 +23,30 @@ parent_files_and_folders = []
 for everything in os.listdir(parent_directory):
 	parent_files_and_folders.append(everything)
 
-#Below are the initial values,
-Initial_Project_Name = properties.DJANGO_PROJECT_NAME
+#Below is the initial value of the project's main app name
 Initial_App_Name = properties.PROJECT_USER_APP
-#do not refer to Initial_Project_Name or Initial_App_Name directly to enable the system revers all work done if there...
-#is an error....rather refer to properties.DJANGO_PROJECT_NAME or properties.PROJECT_USER_APP
+#do not refer to IInitial_App_Name directly to enable the system revers all work done if there...
+#is an error....instead refer to properties.PROJECT_USER_APP
 	
-proj = input("Enter the name of your Django Project: ")
-title = input("What name do you want for your django app: ")
+proj = input("Input title of your Django Project: ")
+title = input("What do you want to name the app that contains the AbstractBaseUser model?: ")
 
-
+#Generate_Secret_key generates random a 60-digit django secret key which excludes forbidden characters
 def Generate_Secret_Key(number=60):
+	ready = True
+	forbidden = ["'", '"', "\\"] #the forbidden characters
+
 	SECRET_KEY = ''.join([secrets.SystemRandom().choice(
 		"{}{}{}".format(string.ascii_letters, string.digits, string.punctuation)
-		) for i in range(number)])
-	return str(SECRET_KEY)
+		) for i in range(number)]) #Secret_Key Generator
+	for characters in forbidden:
+		if characters in SECRET_KEY: #if there is any of the forbidden characters in the Secret_key...
+			ready = False #...change ready to Fals
+	if not ready:
+		#if secret key contains any forbidden_key,....
+		return Generate_Secret_Key()# start this process all over until the generated secret_key contains no forbidden character
+	return SECRET_KEY
+
 
 
 if proj in parent_files_and_folders: #if the inputed project name exists in the parent_directory
@@ -53,13 +62,14 @@ if proj in parent_files_and_folders: #if the inputed project name exists in the 
 
 		print("All Files located, preparing to edit...")
 		
+		#Now write a new settings.py with appropriate input
 		with open(target_settings, 'w') as sets:
 			src = open(base_settings, 'r')
 			for texts in src:
-				if properties.DJANGO_PROJECT_NAME in texts:
-					sets.write(texts.replace(properties.DJANGO_PROJECT_NAME, proj))
-				elif properties.PROJECT_USER_APP in texts:
-					sets.write(texts.replace(properties.PROJECT_USER_APP, title))
+				if 'DjangoAbstractStartPoint' in texts:
+					sets.write(texts.replace('DjangoAbstractStartPoint', proj))
+				elif 'DjangoAbstractBasics' in texts:
+					sets.write(texts.replace('DjangoAbstractBasics', title))
 				elif '__SECRET_KEY__' in texts:
 					sk = Generate_Secret_Key()
 					sets.write(texts.replace('__SECRET_KEY__', sk))
@@ -68,18 +78,20 @@ if proj in parent_files_and_folders: #if the inputed project name exists in the 
 			src.close()
 			print('Edited 1 from 3...')
 
+		#Write a new urls.py with appropriate input
 		with open(target_urls, 'w') as urls:
 			src = open(base_urls, 'r')
 			for texts in src:
-				if properties.DJANGO_PROJECT_NAME in texts:
-					urls.write(texts.replace(properties.DJANGO_PROJECT_NAME, proj))
-				elif properties.PROJECT_USER_APP in texts:
-					urls.write(texts.replace(properties.PROJECT_USER_APP, title))
+				if 'DjangoAbstractStartPoint' in texts:
+					urls.write(texts.replace('DjangoAbstractStartPoint', proj))
+				elif 'DjangoAbstractBasics' in texts:
+					urls.write(texts.replace('DjangoAbstractBasics', title))
 				else:
 					urls.write(texts)
 			src.close()
 			print('Edited 2 from 3...')
 
+		#also edit the properties.py in this folder for future re-usage purposes
 		with open(os.path.join(current_directory, 'properties.py'), 'w') as props:
 			props.write("DJANGO_PROJECT_NAME = '" + proj +"' \n")
 			props.write("PROJECT_USER_APP = '" + title +"' \n")
@@ -90,7 +102,7 @@ if proj in parent_files_and_folders: #if the inputed project name exists in the 
 		print("All Processes Successfully Completed")
 
 	except:
-		print('Something went wrong....Undoing all Processess...')
+		print('Ooooops!! Something has gone horribly wrong....')
 
 
 else:
